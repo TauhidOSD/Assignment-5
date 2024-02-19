18585be33f798eef1b46701a69c5f2d8ed5f929b
Tauhid hossen <tauhidhossen04@gmail.com>
2024-02-19 21:20:04 +0600

    js added

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,13 @@
+
+
+function play(id){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    
+
+    setBackgroundColorById(id);
+}
+function lastSection(){
+    hideElementById('play-ground');
+    showElementById('last-section');
+}
---
+++ scripts/utility.js
@@ -0,0 +1,46 @@
+function hideElementById(elementID){
+    const element =document.getElementById(elementID);
+    element.classList.add('hidden');
+}
+function showElementById(elementID){
+    const element =document.getElementById(elementID);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+  const element =document.getElementById(elementId);
+  element.classList.add('bg-lime-600'); 
+}
+
+const allBtn =document.getElementsByClassName("btn");
+let count=-1;
+let totalSeat=41;
+let totalPrice=-550;
+
+for (const btn of allBtn){
+  btn.addEventListener("click",function(e){
+    
+    count =count+1;
+    totalSeat=totalSeat-1;
+    totalPrice=totalPrice+550;
+
+    
+    // setInnerText("ticket-count",count);
+    // setInnerText("lost-ticket",totalSeat);
+    // setInnerText("total-Price",totalPrice);
+   
+    document.getElementById("ticket-count").innerText=count;
+    document.getElementById("lost-ticket").innerText=totalSeat;
+    document.getElementById("total-Price").innerText=totalPrice;
+    
+  });
+ }
+// function setInnerText(id,value){
+//   document.getElementById(id).innerText=value;
+// }
+
+
+
+
+
+
